fae14d1ca79a3ee69850eb48824557637b343073
Mathew Petro <petromathew@gmail.com>
2022-05-27 17:54:12 -0400

    Removed unused functionality

--- src/components/AutoCompleteInput.js
+++ src/components/AutoCompleteInput.js
@@ -5,8 +5,6 @@ function AutoCompleteInput(props){
     const [textInput, setTextInput] = React.useState("");
     //keep track of the shown suggestions
     const [filteredSuggestions, setFilteredSuggestions] = React.useState([]);
-    //keep track of the index of the active suggestions
-    const [activeSuggestion, setActiveSuggestion] = React.useState(0);
     //keep track of whether to show suggestions or not
     const [showSuggestions, setShowSuggestions] = React.useState(false);
 
@@ -20,22 +18,15 @@ function AutoCompleteInput(props){
 
         setTextInput(userInput);
         setFilteredSuggestions(possibilities);
-        setActiveSuggestion(0); //start the active suggestion from the beginning
         setShowSuggestions(true); //when user is typing show suggestions
     }
 
     //deals with the selection of player from the list
     function selectPlayer(event){
-        //setTextInput(event.target.innerText); //set input to whatever the list item text was
         setFilteredSuggestions([]); //get rid of suggestions
-        setActiveSuggestion(0);
         setShowSuggestions(false);
         props.makeGuess(event.target.innerText);
         setTextInput("");
-    }
-
-    function makeActive(event){
-        setActiveSuggestion(filteredSuggestions.indexOf(event.target.innerText));
     }
 
     return (
@@ -52,11 +43,9 @@ function AutoCompleteInput(props){
                 { showSuggestions && textInput && filteredSuggestions.length ?
                     (
                         <ul className="suggestions">
-                            {filteredSuggestions.map((suggestion, index) => {
-                                let className = "suggestion--inactive";
-                                if (index === activeSuggestion) className = "suggestion--active";
+                            {filteredSuggestions.map((suggestion) => {
                                 return (
-                                    <li className={className} key={suggestion} onClick={selectPlayer} onMouseEnter={makeActive}>
+                                    <li key={suggestion} onClick={selectPlayer}>
                                         {suggestion}
                                     </li>
                                 );
@@ -69,8 +58,7 @@ function AutoCompleteInput(props){
                     )
                     //The above lines are pretty heavy so here is an explanation:
                     //If we have suggestions, show suggestions is on, and we have input, show them, if not, show nothing
-                    //We need to get the active suggestion and display it differently, so we find it and give it a specific class name
-                    //create all the list components of the current suggestion we have and display them                
+                    //create all the list components of the current suggestion we have and display them             
                 }
             </div>
         </div>
